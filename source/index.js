ea13ca56df78c2ce60f250f1d2be7b60f63c868a
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-07-18 22:57:35 -0700

    Making more intelligent logs, configurations

--- source/index.js
+++ source/index.js
@@ -8,6 +8,8 @@ const write = (database) => (path) => {
       return console.error(error)
     }
 
+    console.log(`Change detected to ${path}`)
+
     return database.raw(data)
       .then(() => {
         console.log(`Upserted ${path}`)
@@ -22,9 +24,17 @@ export default function sqlwatcher ({pattern, adapter}) {
   const connection = process.env.DATABASE_URL
   const configuration = {
     adapter,
-    connection
+    connection,
+    pool: {
+      min: 0,
+      max: 10
+    }
   }
   const client = knex(configuration)
+
+  console.log({configuration})
+
+  console.log(`Watching pattern ${pattern}`)
 
   return watch(pattern)
     .on("changed", write(client))
